bf387e96beffb14c5b535eecf83c20659696e440
GABRIEL <gleroy@acheel.com>
2024-10-29 22:22:41 +0100

    feat: added castle moove

--- src/application/services/ChesBoardService/ChessBoardService.ts
+++ src/application/services/ChesBoardService/ChessBoardService.ts
@@ -27,6 +27,29 @@ export class ChessBoardService {
     piece: Piece,
     targetPosition: Position
   ) {
+    const rookMoveIfCastle = (direction: 'left' | 'right') => {
+      let dir;
+      direction === 'left' ? (dir = -1) : (dir = 1);
+      if (
+        piece.notation.toLowerCase() === 'k' &&
+        targetPosition[0] === piece.position[0] + 2 * dir &&
+        !piece.hasMoved
+      ) {
+        const rookInitialPosition = [
+          Math.floor(piece.position[0] + 3.5 * dir),
+          piece.position[1],
+        ];
+        const rookTargetPosition = [piece.position[0] + dir, piece.position[1]];
+        const rookToMove = board[coordToIndex(rookInitialPosition)];
+        rookToMove && (rookToMove.position = rookTargetPosition);
+        board[coordToIndex(rookInitialPosition)] = null;
+        board[coordToIndex(rookTargetPosition)] = rookToMove;
+      }
+    };
+
+    rookMoveIfCastle('left');
+    rookMoveIfCastle('right');
+
     const pieceIndex = coordToIndex(piece.position);
     const targetIndex = coordToIndex(targetPosition);
     piece.position = targetPosition;
