f9e29f0378b6dec9012dba10e725da8eea26e9c8
GABRIEL <gleroy@acheel.com>
2025-04-29 01:50:49 +0200

    fix: build

--- src/interface/App.tsx
+++ src/interface/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FreeChessboardPage from './pages/freeChessBoardPage/FreeChessBoardPage';
+import FreeChessboardPage from './pages/freeChessBoardPage/FreeChessBoardPage.tsx';
 import OnlinePlayersPage from './pages/onlinePlayersPage/OnlinePlayersPage';
 import { AuthContextProvider } from './context/AuthContext';
 import AppLayout from './components/layout/appLayout';
